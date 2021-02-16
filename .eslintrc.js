3b3594b0e1aec5e214850937a7e3ec5560fe64cb
shunshunni <shun_nishitani@me.com>
2021-02-17 08:52:49 +0900

    starting a new project

---
+++ .eslintrc.js
@@ -0,0 +1,117 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'react-hooks',
+    'prefer-arrow'
+  ],
+  root: true,
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define':[
+      'error'
+    ],
+    'no-void':[
+      'error',
+      {
+        allowAsStatement: true,
+      }
+    ],
+    'padding-line-between-statements':[
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'argsIgnorePattern': '_',
+        'ignoreRestSiblings': false,
+        'varsIgnorePattern': '_',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      }
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx', 'ts']
+      }
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'prefer-arrow/prefer-arrow-functions':[
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      }
+    ],
+  },
+  overrides: [
+    {
+      'files': ['*.tsx'],
+      'rules': {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
